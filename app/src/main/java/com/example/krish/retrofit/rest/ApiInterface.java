f9ae4cdaa81bed60ac6b240b52dd03d22118bdad
Krishna Prasad <krishnaprasad97531@gmail.com>
2016-12-14 20:55:29 +0530

    Retrofit
    This is just a sample application to demonstrate retrofit along with
    GSON

---
+++ app/src/main/java/com/example/krish/retrofit/rest/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.example.krish.retrofit.rest;
+
+/**
+ * Created by krish on 12/14/2016.
+ */
+
+import com.example.krish.retrofit.model.MoviesResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
